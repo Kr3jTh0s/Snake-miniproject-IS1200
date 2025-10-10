46205a05b73602f3028bb95eac6b5e8bb1b55714
Kr3jTh0s <alealv04@hotmail.com>
2025-10-10 15:15:30 +0200

    yo

--- snake-game/labmain.c
+++ snake-game/labmain.c
@@ -11,7 +11,11 @@ extern volatile uint8_t* VGA;
 extern volatile uint32_t* VGA_CTRL;
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 volatile int move_snake_mark = 0;		
+=======
+volatile int move_snake_mark = 0;	//Decides if snake is allowed to move	
+>>>>>>> Stashed changes
 =======
 volatile int move_snake_mark = 0;	//Decides if snake is allowed to move	
 >>>>>>> Stashed changes
